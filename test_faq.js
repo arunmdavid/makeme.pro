8d5894b5e092c401a25f3ed8bcdbe27c9fe13aa4
Arun David <arundavid.info@gmail.com>
2021-03-06 20:26:23 +0530

    Create test_faq.js

---
+++ test_faq.js
@@ -0,0 +1,25 @@
+var faqs = [
+  {q: 'What is A?', a: 'A for Apple.'},
+  {q: 'What is B?', a: 'B for Ball.'},
+  {q: 'What is C?', a: 'C for Cat.'},
+];
+var faqElement = document.createElement('div');
+var faqHtml = '<h4>Frequently Asked Questions (FAQ)</h4><hr/>';
+var faqSchemaJson = {"@context":"https://schema.org","@type":"FAQPage","mainEntity":[]};
+faqs.forEach(function(faq, index){
+  faqHtml += '<div><h5>' + faq.q + '</h5>' + '<p>' + faq.a + '</p></div><hr/>';
+  faqSchemaJson.mainEntity.push({
+    "@type":"Question", "name": faq.q,
+    "acceptedAnswer":{
+      "@type":"Answer", "text": faq.a
+    }
+  });
+});
+faqElement.innerHTML=faqHtml;
+var faqSchemaElement = document.createElement('script');
+faqSchemaElement.type = 'application/ld+json';
+faqSchemaElement.text = JSON.stringify(faqSchemaJson);
+if (faqs.length) {
+  document.currentScript.parentNode.insertBefore(faqElement, document.currentScript.nextSibling);
+  document.querySelector('head').appendChild(faqSchemaElement);
+}
