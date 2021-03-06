f3fa9ea277890054e24d8e1486abc0bf657ba1a0
Arun David <arundavid.info@gmail.com>
2021-03-07 00:22:40 +0530

    Update faq.js

--- faq.js
+++ faq.js
@@ -1,4 +1,4 @@
-var faqs = [
+/*var faqs = [
   {q: 'What is A?', a: 'A for Apple.'},
   {q: 'What is B?', a: 'B for Ball.'},
   {q: 'What is C?', a: 'C for Cat.'},
@@ -22,4 +22,28 @@ faqSchemaElement.text = JSON.stringify(faqSchemaJson);
 if (faqs.length) {
   document.currentScript.parentNode.insertBefore(faqElement, document.currentScript.nextSibling);
   document.querySelector('head').appendChild(faqSchemaElement);
-}
+}*/
+var currentScript = document.currentScript;
+fetch('//www.makeme.pro/faq.json').then(function(response) {response.json().then(function(resp) {
+  var faqs = resp.data.faqs;
+  var faqElement = document.createElement('div');
+  var faqHtml = '<h4>Frequently Asked Questions (FAQ)</h4><hr/>';
+  var faqSchemaJson = {"@context":"https://schema.org","@type":"FAQPage","mainEntity":[]};
+  faqs.forEach(function(faq, index){
+    faqHtml += '<div><h5>' + faq.q + '</h5>' + '<p>' + faq.a + '</p></div><hr/>';
+    faqSchemaJson.mainEntity.push({
+      "@type":"Question", "name": faq.q,
+      "acceptedAnswer":{
+        "@type":"Answer", "text": faq.a
+      }
+    });
+  });
+  faqElement.innerHTML=faqHtml;
+  var faqSchemaElement = document.createElement('script');
+  faqSchemaElement.type = 'application/ld+json';
+  faqSchemaElement.text = JSON.stringify(faqSchemaJson);
+  if (faqs.length) {
+    currentScript.parentNode.insertBefore(faqElement, currentScript.nextSibling);
+    document.querySelector('head').appendChild(faqSchemaElement);
+  }
+});});
